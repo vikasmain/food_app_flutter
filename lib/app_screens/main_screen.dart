43077b34b5f5cfc3310b1d6cf4346aa0d2d39518
vikas.bajpayee <vikas.bajpayee@aux.gojek.com>
2019-08-25 12:56:16 +0530

    Add image assets folder

--- lib/app_screens/main_screen.dart
+++ lib/app_screens/main_screen.dart
@@ -3,10 +3,74 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false, // to hide debugDialog
-        title: "Food App",
-        home: Scaffold(
-            appBar: AppBar(title: Text("Food App")), body: MainScreen()));
+    return Center(
+        child: Container(
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Burger",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35.0,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Without Onion",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0,
+                          color: Colors.white),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Pizza",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35.0,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Without Cheese",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0,
+                          color: Colors.white),
+                    )),
+                  ],
+                ),
+                FlightImageAsset()
+              ],
+            )));
+  }
+}
+
+class FlightImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/download.jpeg');
+    Image image = Image(
+      image: assetImage,
+      width: 250.0,
+      height: 250.0,
+    );
+    return Container(
+      child: image,
+    );
   }
 }
