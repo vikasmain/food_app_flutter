1d0171a89b55a449888c719e30d999ddc230f62a
vikas.bajpayee <vikas.bajpayee@aux.gojek.com>
2019-08-25 15:50:05 +0530

    Add listview with text and icons

---
+++ lib/app_screens/list_view_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Food items List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Image.asset("images/download.jpeg",
+                  width: 40.0, height: 40.0, fit: BoxFit.cover),
+              title: Text('Panner'),
+              subtitle: Text("with Matar"),
+              onTap: () {
+                return Dialog(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.fastfood, size: 40.0),
+              title: Text('Burger'),
+            ),
+            ListTile(
+              leading: Image.asset("images/download.jpeg",
+                  width: 40.0, height: 40.0, fit: BoxFit.cover),
+              title: Text('Chaat/Tikki'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void Dialog(BuildContext context) {
+  var alertDialog = AlertDialog(title: Text("Pressed Panner Item"));
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alertDialog;
+      });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'app_screens/main_screen.dart';
+import 'app_screens/list_view_screen.dart';
 
 void main() => runApp(MaterialApp(
       title: "Exploring UI widgets",
-      home: MainScreen(),
+      home: ListScreen(),
     ));
