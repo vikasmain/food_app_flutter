f9b42219d7bc18c8017f7cdd6b84a1c984c94f23
vikas.bajpayee <vikas.bajpayee@aux.gojek.com>
2019-08-25 10:51:02 +0530

    Add Material Component in home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Center(
-          child: Text(
-        "Hello food",
-        textDirection: TextDirection.ltr,
-      )),
-    );
+void main() {
+  runApp(new MaterialApp(
+    title: "Food App", // title of our application in our device
+    home: Material( // we can omit new in new dart version
+      color: Colors.lightBlueAccent,
+      child: Center(
+        child: Text("Hello From Material Component",
+            textDirection: TextDirection.rtl,
+            style: TextStyle(color: Colors.white,fontSize: 40.0,fontStyle: FontStyle.italic)
+        )
+      ),
+    ),
+  ));
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,4 +11,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:food_app/main.dart';
 
 void main() {
+
 }
