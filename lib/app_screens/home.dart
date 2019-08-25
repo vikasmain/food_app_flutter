36d29b4b50fdf901774d44846d9c95ead2296032
vikas.bajpayee <vikas.bajpayee@aux.gojek.com>
2019-08-25 12:23:03 +0530

    Add margin padding to the Container widget

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.lightBlueAccent,
-        alignment: Alignment.center,
-        child: Text("Hello from Container", textDirection: TextDirection.ltr));
+    return Center(
+        child: Container(
+            color: Colors.lightBlueAccent,
+            alignment: Alignment.center,
+//            width: 200.0,
+//            height: 400.0,
+            margin: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Text("Hello from Container",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none, fontSize: 40.0))));
   }
 }
