53158c0105f44930c613ae9c2c8fd70580db3209
vikas.bajpayee <vikas.bajpayee@aux.gojek.com>
2019-08-25 10:36:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## food_app
+
+Food app
+
+## Resources
+more details in this doc
+https://docs.google.com/document/d/1w3KtdzhlVPv4QFdRC7C0MNVxerTxXujuuf_7retv1Ng/edit?usp=sharing
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Center(
+          child: Text(
+        "Hello food",
+        textDirection: TextDirection.ltr,
+      )),
+    );
